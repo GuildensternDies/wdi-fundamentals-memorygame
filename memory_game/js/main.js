15a1139a046d47ec1a07dd69cddb2fa22a6f350d
Brian Weitz <brianweitz@Brians-MacBook-Pro-2.local>
2019-05-17 11:35:52 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,16 @@
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["Queen", "Queen", "King", "King"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped", cardsInPlay[0]);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped", cardsInPlay[1]);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		window.alert("You found a match!");
+	} 
+	else {
+		window.alert("Sorry, try again.");
+	}
+}
